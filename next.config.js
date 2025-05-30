fbd447fd4527ed4fc34cce970f00bc4729bf5131
Leonardo Rodrigues Gomes <leonardo.r.gomes@Hotmail.com>
2025-05-29 23:40:32 -0300

    chore: add webpack alias @ → src

--- next.config.js
+++ next.config.js
@@ -1,14 +1,21 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack(config) {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
+
   async rewrites() {
     return [
       {
-        source: '/api/proxy/:path*',
-        destination: 'http://213.199.38.239:9000/api/:path*',
-        basePath: false
-      }
-    ]
-  }
-}
+        source: "/api/proxy/:path*",
+        destination: "http://213.199.38.239:9000/api/:path*",
+        basePath: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
